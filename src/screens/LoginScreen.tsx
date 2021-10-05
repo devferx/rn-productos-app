bc12a0706f99b51f4c938973441ae2eecf87765c
devferx <ferkki@hotmail.com>
2021-10-05 16:14:37 -0400

    :tada: [ADD] Initial config files

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const LoginScreen = () => {
+  return (
+    <View>
+      <Text style={{color: 'black'}}>Hola desde Login Screen</Text>
+    </View>
+  );
+};
