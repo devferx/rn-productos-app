e839eae7f59b49335dc22b65e73f9ca845a47b98
devferx <ferkki@hotmail.com>
2021-10-06 09:32:51 -0400

    :dizzy: [ADD] React logo animation

--- src/components/WhiteLogo.tsx
+++ src/components/WhiteLogo.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import {Image, StyleSheet, View} from 'react-native';
+import React, {useRef} from 'react';
+import {Animated, Image, StyleSheet, TouchableOpacity} from 'react-native';
 
 export const WhiteLogo = () => {
+  const rotateAnim = useRef(new Animated.Value(0)).current;
+
+  const handleTap = () => {
+    Animated.timing(rotateAnim, {
+      toValue: 1,
+      duration: 600,
+      useNativeDriver: true,
+    }).start(() => rotateAnim.setValue(0));
+  };
+
+  const interpolateRotating = rotateAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.image}
-        source={require('../assets/react-logo-white.png')}
-      />
-    </View>
+    <TouchableOpacity onPress={handleTap} activeOpacity={0.9}>
+      <Animated.View
+        style={{
+          ...styles.container,
+          transform: [{rotate: interpolateRotating}],
+        }}>
+        <Image
+          style={styles.image}
+          source={require('../assets/react-logo-white.png')}
+        />
+      </Animated.View>
+    </TouchableOpacity>
   );
 };
 
