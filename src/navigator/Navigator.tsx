75047e3ea0180e4a0f89d771fe225131c9273dec
devferx <ferkki@hotmail.com>
2021-11-06 23:37:41 -0400

    :construction: [ADD] ProductsNavigator

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -6,6 +6,7 @@ import {LoginScreen} from '../screens/LoginScreen';
 import {ProtectedScreen} from '../screens/ProtectedScreen';
 import {RegisterScreen} from '../screens/RegisterScreen';
 import {LoadingScreen} from '../screens/LoadingScreen';
+import {ProductsNavigator} from './ProductsNavigator';
 
 const Stack = createStackNavigator();
 
@@ -28,7 +29,13 @@ export const Navigator = () => {
           <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
         </>
       ) : (
-        <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
+        <>
+          <Stack.Screen
+            name="ProductsNavigator"
+            component={ProductsNavigator}
+          />
+          <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
+        </>
       )}
     </Stack.Navigator>
   );
